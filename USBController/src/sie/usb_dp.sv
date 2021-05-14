78ca43d58e5876ca1d2eaa79d6e37ad021dd6c79
7FM <41307817+7FM@users.noreply.github.com>
2021-05-14 18:39:20 +0200

    differential pair: add additional flopping stage & keep track of previous OUT_EN values to be in sync with the corresponding signal

--- USBController/src/sie/usb_dp.sv
+++ USBController/src/sie/usb_dp.sv
@@ -21,8 +21,28 @@ module usb_dp(
 
     logic inP, inP_negedge, inN;
 
+`ifdef USB_DP_ADD_NEGEDGE_SYNC_BLOCK
+`undef USB_DP_ADD_NEGEDGE_SYNC_BLOCK
+`endif
+
+`ifndef DP_REGISTERED_INPUT
+    `define USB_DP_ADD_NEGEDGE_SYNC_BLOCK
+    localparam DOUBLE_FLOP_SHIFT_REG_LENGTH = 2;
+`else
+`ifdef RUN_SIM
+    `define USB_DP_ADD_NEGEDGE_SYNC_BLOCK
+    localparam DOUBLE_FLOP_SHIFT_REG_LENGTH = 2;
+`else
+    localparam DOUBLE_FLOP_SHIFT_REG_LENGTH = 1;
+`endif
+`endif
+
+    // inP_negedge will always be an register no matter the settings -> we need one double flopping stage less
+    localparam DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH = ((DOUBLE_FLOP_SHIFT_REG_LENGTH-1) == 0 ? 1 : DOUBLE_FLOP_SHIFT_REG_LENGTH-1);
+
     // Prevent propagation of meta stability by double flopping
-    logic doubleFlopP, doubleFlopP_negedge, doubleFlopN;
+    logic [DOUBLE_FLOP_SHIFT_REG_LENGTH-1:0] doubleFlopP, doubleFlopN;
+    logic [DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH-1:0] doubleFlopP_negedge;
 
 `ifndef RUN_SIM
 
@@ -63,69 +83,61 @@ module usb_dp(
 `endif
     );
 
-`ifdef DP_REGISTERED_INPUT
-    assign doubleFlopP = inP;
-    assign doubleFlopN = inN;
-    assign doubleFlopP_negedge = inP_negedge;
-`else
-    assign inP_negedge = inP;
-`endif
-
 `else // SIMULATION CASE
-    // Always double flop in simulation
-
     // Tristate output logic
     assign pinP_OUT = OUT_EN ? dataOutP : 1'bz;
     assign pinN_OUT = OUT_EN ? dataOutN : 1'bz;
 
     assign inP = pinP;
-    assign inP_negedge = pinP;
     assign inN = pinN;
 `endif
 
+    localparam OUT_EN_SHIFT_REG_LENGTH = 2;
+    logic [OUT_EN_SHIFT_REG_LENGTH-1:0] outEnShiftReg;
     initial begin
-`ifndef RUN_SIM
-`ifndef DP_REGISTERED_INPUT
-        doubleFlopP = 1'b1;
-        doubleFlopP_negedge = 1'b1;
-        doubleFlopN = 1'b0;
-`endif
-`else
-        doubleFlopP = 1'b1;
-        doubleFlopP_negedge = 1'b1;
-        doubleFlopN = 1'b0;
-`endif
+        outEnShiftReg = {OUT_EN_SHIFT_REG_LENGTH{1'b0}};
+        doubleFlopP = {DOUBLE_FLOP_SHIFT_REG_LENGTH{1'b1}};
+        doubleFlopP_negedge = {DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH{1'b1}};
+        doubleFlopN = {DOUBLE_FLOP_SHIFT_REG_LENGTH{1'b0}};
         dataInP = 1'b1;
         dataInP_negedge = 1'b1;
         dataInN = 1'b0;
+`ifdef USB_DP_ADD_NEGEDGE_SYNC_BLOCK
+        inP_negedge = 1'b1;
+`endif
     end
 
-`ifndef RUN_SIM
-`ifndef DP_REGISTERED_INPUT
+`ifdef USB_DP_ADD_NEGEDGE_SYNC_BLOCK
     always_ff @(negedge clk48) begin
-        doubleFlopP_negedge <= inP_negedge;
+        inP_negedge <= inP;
     end
 `endif
-`else // SIMULATION CASE
-    always_ff @(negedge clk48) begin
-        doubleFlopP_negedge <= inP_negedge;
-    end
-`endif
+
 
     always_ff @(posedge clk48) begin
-`ifndef RUN_SIM
-`ifndef DP_REGISTERED_INPUT
-        doubleFlopP <= inP;
-        doubleFlopN <= inN;
-`endif
-`else
-        doubleFlopP <= inP;
-        doubleFlopN <= inN;
-`endif
+        outEnShiftReg <= {outEnShiftReg[OUT_EN_SHIFT_REG_LENGTH-2:0], OUT_EN};
 
-        dataInP <= OUT_EN ? 1'b1 : doubleFlopP;
-        dataInP_negedge <= OUT_EN ? 1'b1 : doubleFlopP_negedge;
-        dataInN <= OUT_EN ? 1'b0 : doubleFlopN;
+        doubleFlopP[0] <= inP;
+        doubleFlopN[0] <= inN;
+        doubleFlopP_negedge[0] <= inP_negedge;
+
+        dataInP <= outEnShiftReg[OUT_EN_SHIFT_REG_LENGTH-1] ? 1'b1 : doubleFlopP[DOUBLE_FLOP_SHIFT_REG_LENGTH-1];
+        dataInP_negedge <= outEnShiftReg[OUT_EN_SHIFT_REG_LENGTH-1] ? 1'b1 : doubleFlopP_negedge[DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH-1];
+        dataInN <= outEnShiftReg[OUT_EN_SHIFT_REG_LENGTH-1] ? 1'b0 : doubleFlopN[DOUBLE_FLOP_SHIFT_REG_LENGTH-1];
     end
+
+    generate
+        if (DOUBLE_FLOP_SHIFT_REG_LENGTH > 1) begin
+            always_ff @(posedge clk48) begin
+                doubleFlopP[DOUBLE_FLOP_SHIFT_REG_LENGTH-1:1] <= doubleFlopP[DOUBLE_FLOP_SHIFT_REG_LENGTH-2:0];
+                doubleFlopN[DOUBLE_FLOP_SHIFT_REG_LENGTH-1:1] <= doubleFlopN[DOUBLE_FLOP_SHIFT_REG_LENGTH-2:0];
+            end
+        end
+        if (DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH > 1) begin
+            always_ff @(posedge clk48) begin
+                doubleFlopP_negedge[DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH-1:1] <= doubleFlopP_negedge[DOUBLE_FLOP_NEGEDGE_SHIFT_REG_LENGTH-2:0];
+            end
+        end
+    endgenerate
 
 endmodule
