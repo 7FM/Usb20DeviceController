2edc82086f86081d7b0d63d17dc0efc73c70beec
7FM <41307817+7FM@users.noreply.github.com>
2021-05-16 17:05:23 +0200

    immediate calculation results of CRC must be forwarded to meet timing requirements, else a wrong CRC will be sent in usb_tx

--- USBController/src/sie/usb_crc.sv
+++ USBController/src/sie/usb_crc.sv
@@ -10,16 +10,16 @@ module usb_crc(
     output logic [15:0] crc
 );
 
-    logic [15:0] crcBuf;
+    logic [15:0] crcBuf, next_crcBuf;
     logic useCRC16, crc5_in, crc16_in, crcX_in, crc5_valid, crc16_valid;
 
     assign crc5_in = crcBuf[4] ^ data;
     assign crc16_in = crcBuf[15] ^ data;
     assign crcX_in = useCRC16 ? crc16_in : crc5_in;
 
-    //TODO wtf this is send in MSb and not LSb as all other fields!
     // When the last bit of the checked field is sent, the CRC in the generator is inverted and sent to the checker MSb first
-    assign crc = ~crcBuf;
+    // Due to timing requirements, we need to forward the calculation result instead of the register itself
+    assign crc = ~next_crcBuf;
 
     // When the last bit of the CRC is received by the checker and no errors have occurred, the remainder will be equal to the polynomial residual.
     localparam crc16_residual = 16'b1000_0000_0000_1101;
@@ -28,28 +28,37 @@ module usb_crc(
     assign crc5_valid = crcBuf[4:0] == crc5_residual;
     assign validCRC = useCRC16 ? crc16_valid : crc5_valid;
 
-    always_ff @(posedge clk12) begin
+    always_comb begin
+        next_crcBuf = crcBuf;
         // CRC calculation magic:
-
-        // For CRC generation and checking, the shift registers in the generator and checker are seeded with an allones pattern. For each data bit sent or received, the high order bit of the current remainder is XORed with
+        // For each data bit sent or received, the high order bit of the current remainder is XORed with
         // the data bit and then the remainder is shifted left one bit and the low-order bit set to zero. If the result of
         // that XOR is one, then the remainder is XORed with the generator polynomial.
-        if (RST) begin
-            crcBuf <= {16{1'b1}};
-            useCRC16 <= rxUseCRC16;
-        end else if (VALID) begin
+        if (VALID) begin
             // Shift and XOR with polynomial if crcX_in is 1 -> XOR with crcX_in
             // CRC5  polynomial: 0b0000_0000_0000_0101
             // CRC16 polynomial: 0b1000_0000_0000_0101
             // -> lower bits are identical
             // -> as we ignore the upper most bits for CRC5 we can always xor at the locations of CRC16 polynomial with an 1
-            crcBuf[15:0] <= {
+            next_crcBuf = {
                 crcBuf[14] ^ crcX_in,
                 crcBuf[13:2],
                 crcBuf[1] ^ crcX_in,
                 crcBuf[0],
                 crcX_in
             };
+        end
+    end
+
+    always_ff @(posedge clk12) begin
+        // CRC calculation magic:
+
+        // For CRC generation and checking, the shift registers in the generator and checker are seeded with an allones pattern.
+        if (RST) begin
+            crcBuf <= {16{1'b1}};
+            useCRC16 <= rxUseCRC16;
+        end else begin
+            crcBuf <= next_crcBuf;
         end
     end
 
