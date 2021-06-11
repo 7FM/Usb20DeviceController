181dfffe5639a795d67427f51fe08018c165847d
7FM <41307817+7FM@users.noreply.github.com>
2021-06-11 16:19:53 +0200

    move usb bit stuffing wrapper to its own file

---
+++ USBController/src/sie/usb_bit_stuffing_wrapper.sv
@@ -0,0 +1,21 @@
+module usb_bit_stuffing_wrapper (
+    input logic clk12,
+    input logic RST,
+    input logic outEN,
+    input logic dataIn,
+    output logic ready_valid,
+    output logic dataOut,
+    output logic error
+);
+
+    usb_bit_unstuff unstuffer(
+        .clk12(clk12),
+        .RST(RST),
+        .valid(ready_valid),
+        .data(outEN && !ready_valid ? 1'b0 : dataIn),
+        .error(error)
+    );
+
+    assign dataOut = ready_valid ? dataIn : 1'b0;
+
+endmodule
