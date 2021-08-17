4fb924ff6fd960bc55281cb883efe69eeeac741f
7FM <41307817+7FM@users.noreply.github.com>
2021-08-17 21:26:46 +0200

    fix transaction start buffer

--- USBController/include/usb_packet_pkg.sv
+++ USBController/include/usb_packet_pkg.sv
@@ -123,8 +123,8 @@ package usb_packet_pkg;
 
     localparam _MAX_INIT_TRANS_PACKET_LEN = _SOF_LENGTH > _TOKEN_LENGTH ? _SOF_LENGTH : _TOKEN_LENGTH;
     // Round buffer length up to the next byte boundary!
-    localparam INIT_TRANS_PACKET_BUF_LEN = 2 ** $clog2(_MAX_INIT_TRANS_PACKET_LEN);
-    localparam INIT_TRANS_PACKET_BUF_BYTE_COUNT = INIT_TRANS_PACKET_BUF_LEN / 8;
+    localparam INIT_TRANS_PACKET_BUF_BYTE_COUNT = (_MAX_INIT_TRANS_PACKET_LEN + 8 - 1) / 8;
+    localparam INIT_TRANS_PACKET_BUF_LEN = INIT_TRANS_PACKET_BUF_BYTE_COUNT * 8 - 1;
 
 endpackage
 
