0943d35d5a617d6b198e6921ef2f1da12032c31a
7FM <41307817+7FM@users.noreply.github.com>
2021-08-31 20:12:58 +0200

    sim_top: implement helper function to issue multiple read request to read an entire descriptor

--- USBController/sim_src/sim_top.cpp
+++ USBController/sim_src/sim_top.cpp
@@ -9,9 +9,9 @@
 #include "Vsim_top__Syms.h" // all headers to access exposed internal signals
 
 #include "common/VerilatorTB.hpp"
-#include "common/usb_utils.hpp" // Utils to create & read a usb packet
-#include "common/usb_packets.hpp"
 #include "common/usb_descriptors.hpp"
+#include "common/usb_packets.hpp"
+#include "common/usb_utils.hpp" // Utils to create & read a usb packet
 
 static std::atomic_bool forceStop = false;
 
@@ -97,7 +97,6 @@ static bool sendStuff(UsbTopSim &sim, std::function<void()> fillSendData) {
 }
 
 static bool receiveStuff(UsbTopSim &sim, const char *errMsg) {
-
     // Enable timeout for receiving a response
     sim.rxState.reset();
     sim.rxState.enableTimeout = true;
@@ -206,7 +205,7 @@ class OutTransaction {
     }
 };
 
-static void printResponse(const std::vector<uint8_t>& response) {
+static void printResponse(const std::vector<uint8_t> &response) {
     if (response.empty()) {
         std::cerr << "No response received!" << std::endl;
         return;
@@ -224,6 +223,132 @@ static void printResponse(const std::vector<uint8_t>& response) {
     }
 }
 
+static bool readItAll(std::vector<uint8_t> &result, UsbTopSim &sim, int addr, int readSize, int ep0MaxDescriptorSize) {
+    result.clear();
+
+    InTransaction getDesc;
+    getDesc.inTokenPacket.token = PID_IN_TOKEN;
+    getDesc.inTokenPacket.addr = addr;
+    getDesc.inTokenPacket.endpoint = 0;
+    getDesc.inTokenPacket.crc = 0b11111; // Should be a dont care!
+    getDesc.handshakeToken = PID_HANDSHAKE_ACK;
+
+    do {
+        std::cout << "Send Input transaction packet" << std::endl;
+        bool failed = getDesc.send(sim);
+        printResponse(sim.rxState.receivedData);
+
+        if (failed) {
+            return true;
+        }
+
+        // Skip PID
+        for (int i = 1; i < sim.rxState.receivedData.size(); ++i) {
+            result.push_back(sim.rxState.receivedData[i]);
+        }
+
+        readSize -= sim.rxState.receivedData.size() - 1;
+
+        if (sim.rxState.receivedData.size() - 1 != ep0MaxDescriptorSize && readSize > 0) {
+            std::cerr << "ERROR: Received non max sized packet but still expecting data to come!" << std::endl;
+            return true;
+        }
+
+        //TODO is a sim.reset needed?
+
+    } while (readSize > 0);
+
+    return false;
+}
+
+static bool readDescriptor(std::vector<uint8_t> &result, UsbTopSim &sim, DescriptorType descType, uint8_t descIdx, int &ep0MaxDescriptorSize, uint8_t addr) {
+    bool failed = false;
+
+    OutTransaction setupTrans;
+    setupTrans.outTokenPacket.token = PID_SETUP_TOKEN;
+    setupTrans.outTokenPacket.addr = addr;
+    setupTrans.outTokenPacket.endpoint = 0;
+    setupTrans.outTokenPacket.crc = 0b11111; // Should be a dont care!
+
+    SetupPacket packet;
+    std::memset(&packet, 0, sizeof(packet));
+    // Request type
+    packet.request = DEVICE_GET_DESCRIPTOR;
+
+    // Descriptor type
+    packet.wValueMsB = descType;
+    // Descriptor index;
+    packet.wValueLsB = descIdx;
+    // Zero or Language ID
+    packet.wIndexLsB = packet.wIndexMsB = 0;
+    // Device Descriptor length unknown, so lets first get only the first 8 bytes
+    packet.wLengthLsB = 8;
+    packet.wLengthMsB = 0;
+
+    setupTrans.dataPacket.push_back(PID_DATA0);
+    fillVector(setupTrans.dataPacket, packet);
+
+    std::cout << "Send Setup transaction packet" << std::endl;
+    failed = setupTrans.send(sim);
+    printResponse(sim.rxState.receivedData);
+    //TODO handle handshake!
+
+    if (failed) {
+        return true;
+    }
+
+    failed = readItAll(result, sim, addr, 8, 8);
+
+    if (result.size() != 8) {
+        std::cerr << "Pre read first 8 bytes of descriptor error but got only: " << result.size() << " bytes!" << std::endl;
+        failed = true;
+    }
+
+    if (failed) {
+        return true;
+    }
+
+    int descriptorSize = result[0];
+
+    if (descType == DESC_DEVICE) {
+        ep0MaxDescriptorSize = result[7];
+    }
+
+    if (descriptorSize > 8) {
+        // We need to fetch the remaining data too!
+        packet.wLengthLsB = descriptorSize;
+
+        // New setup transaction to set the correct size that we want to read!
+        setupTrans.dataPacket.clear();
+        setupTrans.dataPacket.push_back(PID_DATA0);
+        fillVector(setupTrans.dataPacket, packet);
+
+        std::cout << "Send Setup transaction packet" << std::endl;
+        failed = setupTrans.send(sim);
+        printResponse(sim.rxState.receivedData);
+        //TODO handle handshake!
+
+        if (failed) {
+            return true;
+        }
+
+        failed = readItAll(result, sim, addr, descriptorSize, ep0MaxDescriptorSize);
+
+        if (result.size() != descriptorSize) {
+            std::cerr << "Error expected descriptor result size: " << descriptorSize << " but got: " << result.size() << std::endl;
+            failed = true;
+        }
+
+        if (failed) {
+            return true;
+        }
+    }
+
+    std::cout << "Successfully received a " << descTypeToString(descType) << " Descriptor!" << std::endl;
+
+    return false;
+}
+
 /******************************************************************************/
 int main(int argc, char **argv) {
     std::signal(SIGINT, signalHandler);
@@ -236,61 +361,28 @@ int main(int argc, char **argv) {
 
     bool failed = false;
 
-    // Test 1: Setup transaction: request the device descriptor
-    OutTransaction setupTrans;
-    setupTrans.outTokenPacket.token = PID_SETUP_TOKEN;
-    setupTrans.outTokenPacket.addr = 0;
-    setupTrans.outTokenPacket.endpoint = 0;
-    setupTrans.outTokenPacket.crc = 0b11111; // Should be a dont care!
+    std::vector<uint8_t> result;
+    int ep0MaxPacketSize = 0;
+    int addr = 0;
+    failed |= readDescriptor(result, sim, DESC_DEVICE, 0, ep0MaxPacketSize, addr);
 
-    SetupPacket packet;
-    std::memset(&packet, 0, sizeof(packet));
-    // Request type
-    packet.request = DEVICE_GET_DESCRIPTOR;
-
-    // Descriptor type
-    packet.wValueMsB = DESC_DEVICE;
-    // Descriptor index;
-    packet.wValueLsB = 0;
-    // Zero or Language ID
-    packet.wIndexLsB = packet.wIndexMsB = 0;
-    // Descriptor length, is unknown but can be determined by reading the first 8 bytes
-    packet.wLengthLsB = 8;
-    packet.wLengthMsB = 0;
-
-    setupTrans.dataPacket.push_back(PID_DATA0);
-    fillVector(setupTrans.dataPacket, packet);
-
-    std::cout << "Send Setup transaction packet" << std::endl;
-    failed |= setupTrans.send(sim);
-    printResponse(sim.rxState.receivedData);
-    //TODO check results
-
-    sim.reset();
-
-    InTransaction getDesc;
-    getDesc.inTokenPacket.token = PID_IN_TOKEN;
-    getDesc.inTokenPacket.addr = 0;
-    getDesc.inTokenPacket.endpoint = 0;
-    getDesc.inTokenPacket.crc = 0b11111; // Should be a dont care!
-    getDesc.handshakeToken = PID_HANDSHAKE_ACK;
-
-    std::cout << "Send Input transaction packet" << std::endl;
-    failed |= getDesc.send(sim);
-    printResponse(sim.rxState.receivedData);
-    //TODO check results
-
-    DeviceDescriptor deviceDescriptor;
-    std::memset(&deviceDescriptor, 0, sizeof(deviceDescriptor));
-    uint8_t *rawDevDesc = reinterpret_cast<uint8_t *>(&deviceDescriptor);
-    for (int i = 1; i < sim.rxState.receivedData.size(); ++i) {
-        *rawDevDesc = sim.rxState.receivedData[i];
-        ++rawDevDesc;
+    if (result.size() != 12) {
+        failed = true;
+        goto exitAndCleanup;
     }
-    std::cout << "Device Descriptor:" << std::endl;
-    prettyPrintDeviceDescriptor(deviceDescriptor);
 
-    sim.reset();
+    {
+        DeviceDescriptor deviceDescriptor;
+        std::memset(&deviceDescriptor, 0, sizeof(deviceDescriptor));
+        uint8_t *rawDevDesc = reinterpret_cast<uint8_t *>(&deviceDescriptor);
+        for (auto d : result) {
+            *rawDevDesc = d;
+            ++rawDevDesc;
+        }
+        std::cout << "Device Descriptor:" << std::endl;
+        prettyPrintDeviceDescriptor(deviceDescriptor);
+        //TODO check content
+    }
 
 exitAndCleanup:
 
