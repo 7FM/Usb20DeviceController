288538eed97cdb84260664259d047521ad331f1d
7FM <41307817+7FM@users.noreply.github.com>
2021-10-09 23:17:33 +0200

    change usb_rx cdc strategy: use a FIFO

--- USBController/src/sie/rx/cdc_rx.sv
+++ USBController/src/sie/rx/cdc_rx.sv
@@ -13,7 +13,7 @@ module cdc_rx(
     output logic [7:0] rxData_o,
     output logic keepPacket_o
 );
-
+/*
     cdc_2phase_sync #(
         .DATA_WID(8 + 1 + 1)
     ) dataSyncer (
@@ -25,6 +25,28 @@ module cdc_rx(
         .clk2(clk2),
         .ready_i(rxAcceptNewData_i),
         .valid_o(rxDataValid_o),
+        .data_o({rxData_o, rxIsLastByte_o, keepPacket_o})
+    );
+*/
+
+    logic fifoFull;
+    assign rxAcceptNewData_o = !fifoFull;
+
+    logic fifoEmpty;
+    assign rxDataValid_o = !fifoEmpty;
+
+    ASYNC_FIFO #(
+        .ADDR_WID(2),
+        .DATA_WID(10)
+    ) cdcFifo (
+        .w_clk_i(clk1),
+        .dataValid_i(rxDataValid_i),
+        .data_i({rxData_i, rxIsLastByte_i, keepPacket_i}),
+        .full_o(fifoFull),
+
+        .r_clk_i(clk2),
+        .popData_i(rxAcceptNewData_i),
+        .empty_o(fifoEmpty),
         .data_o({rxData_o, rxIsLastByte_o, keepPacket_o})
     );
 
