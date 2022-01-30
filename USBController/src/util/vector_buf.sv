a45374cdef91ca780a607af99eef3508f3c5c2d7
7FM <41307817+7FM@users.noreply.github.com>
2022-01-30 21:15:21 +0100

    add a new parameter to vector_buf

--- USBController/src/util/vector_buf.sv
+++ USBController/src/util/vector_buf.sv
@@ -1,7 +1,8 @@
 module vector_buf#(
     parameter DATA_WID = 8,
     parameter BUF_SIZE,
-    parameter INITIALIZE_BUF_IDX = 0
+    parameter INITIALIZE_BUF_IDX = 0,
+    parameter USE_INVERTED_INDEXING_SCHEME = 0
 )(
     input logic clk_i,
     input logic rst_i,
@@ -20,24 +21,53 @@ module vector_buf#(
     generate
         if (INITIALIZE_BUF_IDX) begin
             initial begin
-                bufIdx = {IDX_WID{1'b0}};
+                if (USE_INVERTED_INDEXING_SCHEME) begin
+                    bufIdx = BUF_SIZE[IDX_WID-1:0];                
+                end else begin
+                    bufIdx = {IDX_WID{1'b0}};
+                end
             end
         end
-    endgenerate
 
-    assign isFull_o = bufIdx == BUF_SIZE[IDX_WID-1:0];
-    assign nextBufIdx = isFull_o ? bufIdx : bufIdx + 1;
+        if (USE_INVERTED_INDEXING_SCHEME) begin
+            assign isFull_o = bufIdx == 0;
+            assign nextBufIdx = isFull_o ? bufIdx : bufIdx - 1;
+        end else begin
+            assign isFull_o = bufIdx == BUF_SIZE[IDX_WID-1:0];
+            assign nextBufIdx = isFull_o ? bufIdx : bufIdx + 1;
+        end
+    endgenerate
 
     logic handshake;
     assign handshake = !isFull_o && dataValid_i;
 
-    always_ff @(posedge clk_i) begin
-        if (rst_i) begin
-            bufIdx <= {IDX_WID{1'b0}};
-        end else if (handshake) begin
-            bufIdx <= nextBufIdx;
-            buffer_o[bufIdx * DATA_WID +: DATA_WID] <= data_i;
+    generate
+        genvar i;
+
+        if (USE_INVERTED_INDEXING_SCHEME) begin
+            logic [DATA_WID*BUF_SIZE - 1:0] buffer_reversed;
+            for (i = 0; i < BUF_SIZE; i++) begin
+                assign buffer_o[i * DATA_WID +: DATA_WID] = buffer_reversed[(BUF_SIZE - 1 - i) * DATA_WID - 1 -: DATA_WID];
+            end
+
+            always_ff @(posedge clk_i) begin
+                if (rst_i) begin
+                    bufIdx <= BUF_SIZE[IDX_WID-1:0];
+                end else if (handshake) begin
+                    bufIdx <= nextBufIdx;
+                    buffer_reversed[nextBufIdx * DATA_WID - 1 -: DATA_WID] <= data_i;
+                end
+            end
+        end else begin
+            always_ff @(posedge clk_i) begin
+                if (rst_i) begin
+                    bufIdx <= {IDX_WID{1'b0}};
+                end else if (handshake) begin
+                    bufIdx <= nextBufIdx;
+                    buffer_o[bufIdx * DATA_WID +: DATA_WID] <= data_i;
+                end
+            end
         end
-    end
+    endgenerate
 
 endmodule
