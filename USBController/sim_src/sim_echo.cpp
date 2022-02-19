9586440ab6588261e824e649a3ed3237e6524523
7FM <41307817+7FM@users.noreply.github.com>
2022-02-19 22:55:56 +0100

    improve compare vec

--- USBController/sim_src/sim_echo.cpp
+++ USBController/sim_src/sim_echo.cpp
@@ -125,12 +125,13 @@ static bool compareVec(const std::vector<uint8_t> &expected, const std::vector<u
         failed = true;
     }
 
+    IosFlagSaver _(std::cout);
     int minSize = std::min(got.size(), expected.size());
     for (int i = 0; i < minSize; ++i) {
         if (got[i] != expected[i]) {
             failed = true;
-            std::cout << dataErrMsg << i << std::endl;
-            std::cout << "  Expected: " << expected[i] << " but got: " << got[i] << std::endl;
+            std::cout << dataErrMsg << std::dec << i << std::endl;
+            std::cout << "  Expected: 0x" << std::hex << static_cast<int>(expected[i]) << " but got: 0x" << static_cast<int>(got[i]) << std::endl;
         }
     }
     return failed;
