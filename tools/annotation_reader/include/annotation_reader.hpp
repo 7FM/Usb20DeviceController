82d8d7e5c7a8d2888ba13875ec3442cdb8a58b7f
7FM <41307817+7FM@users.noreply.github.com>
2022-04-01 00:24:14 +0200

    annotation_reader: split header into hpp & cpp

--- tools/annotation_reader/include/annotation_reader.hpp
+++ tools/annotation_reader/include/annotation_reader.hpp
@@ -5,8 +5,6 @@
 #include <string>
 #include <vector>
 
-#include <iostream>
-
 enum UsbPacket {
     NONE,
     SOF,
@@ -17,23 +15,6 @@ enum UsbPacket {
     HANDSHAKE,
 };
 
-#define STRINGIFY_CASE(x) \
-    case (x):             \
-        return #x
-
-static const char *usbPacketToStr(UsbPacket p) {
-    switch (p) {
-        STRINGIFY_CASE(NONE);
-        STRINGIFY_CASE(SOF);
-        STRINGIFY_CASE(SETUP);
-        STRINGIFY_CASE(IN);
-        STRINGIFY_CASE(OUT);
-        STRINGIFY_CASE(DATA);
-        STRINGIFY_CASE(HANDSHAKE);
-    }
-    return "INVALID";
-}
-
 struct Packet {
     uint64_t startTime, endTime;
     UsbPacket type;
@@ -41,12 +22,6 @@ struct Packet {
 
     friend std::ostream &operator<<(std::ostream &s, const Packet& p);
 };
-
-std::ostream &operator<<(std::ostream &s, const Packet& p) {
-    s << usbPacketToStr(p.type) << " start: " << p.startTime << " end: " << p.endTime << " ignore: " << p.ignore;
-    return s;
-}
-
 
 class annotation_reader {
   public:
@@ -59,83 +34,7 @@ class annotation_reader {
         return in.good();
     }
 
-    void parse(std::vector<Packet> &packets) {
-        std::string line;
-        Packet currentPacket;
-        currentPacket.type = NONE;
-
-        while (in.good()) {
-            std::getline(in, line);
-
-            if (line.empty()) {
-                continue;
-            }
-
-#define SEARCH_TERM " USB packet: Packet fields: "
-            auto constexpr searchTermSize = sizeof(SEARCH_TERM) / sizeof(SEARCH_TERM[0]);
-#define DELIMITER_TERM ": "
-            auto constexpr delimiterTermSize = sizeof(DELIMITER_TERM) / sizeof(DELIMITER_TERM[0]);
-            auto it = line.find(SEARCH_TERM);
-            if (it != std::string::npos) {
-                std::string region = line.substr(0, it);
-
-                auto regionSplit = region.find('-');
-
-                if (regionSplit == std::string::npos) {
-                    // TODO warn?
-                    continue;
-                }
-
-                uint64_t regionStart = std::stoull(region.substr(0, regionSplit));
-                uint64_t regionEnd = std::stoull(region.substr(regionSplit + 1));
-                regionStart = static_cast<decltype(regionStart)>(regionStart * 1'000'000'000.0 / (2 * 48'000'000));
-                regionEnd = static_cast<decltype(regionEnd)>(regionEnd * 1'000'000'000.0 / (2 * 48'000'000));
-
-                line = line.substr(it + searchTermSize - 1);
-                it = line.find(DELIMITER_TERM);
-
-                if (it != std::string::npos) {
-                    std::string packetField = line.substr(0, it);
-                    if (packetField == "SYNC") {
-                        if (currentPacket.type != NONE) {
-                            packets.push_back(currentPacket);
-                        }
-                        currentPacket.type = NONE;
-                        currentPacket.startTime = regionStart;
-                        currentPacket.ignore = false;
-                    } else if (packetField == "PID") {
-                        if (currentPacket.type == NONE) {
-                            std::string pid = line.substr(it + delimiterTermSize - 1);
-                            currentPacket.type = NONE;
-
-                            if (pid == "ACK" || pid == "NAK" || pid == "STALL" || pid == "NYET") {
-                                currentPacket.type = HANDSHAKE;
-                            } else if (pid == "IN") {
-                                currentPacket.type = IN;
-                            } else if (pid == "OUT") {
-                                currentPacket.type = OUT;
-                            } else if (pid == "SETUP") {
-                                currentPacket.type = SETUP;
-                            } else if (pid == "SOF") {
-                                currentPacket.type = SOF;
-                            } else if (pid.starts_with("DATA")) {
-                                currentPacket.type = DATA;
-                            } else {
-                                std::cout << "WARNING: unknown PID: " << pid << std::endl;
-                            }
-                        }
-                    } else {
-                        // They dont really care about us
-                    }
-                    currentPacket.endTime = regionEnd;
-                }
-            }
-        }
-
-        if (currentPacket.type != NONE) {
-            packets.push_back(currentPacket);
-        }
-    }
+    void parse(std::vector<Packet> &packets);
 
   private:
     std::ifstream in;
