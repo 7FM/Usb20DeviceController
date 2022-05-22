ae265ec2ea547bf280f333b40d8d8eeb65e499d0
7FM <41307817+7FM@users.noreply.github.com>
2022-05-22 12:11:38 +0200

    complete vcd_concat

--- tools/vcd_concat/src/main.cpp
+++ tools/vcd_concat/src/main.cpp
@@ -14,11 +14,45 @@ static void printHelp() {
               << std::endl;
 }
 
-struct DummySignalWrapper {
+struct TranslatingSignalWrapper {
+    TranslatingSignalWrapper(std::string &newVcdAlias)
+        : vcdAlias(newVcdAlias), value(), valueChanged(false) {}
+
     bool handleValueChange(
-        const vcd_reader<DummySignalWrapper>::ValueUpdate &value) {
+        const vcd_reader<TranslatingSignalWrapper>::ValueUpdate &value) {
+        if (this->value != value.valueStr) {
+            this->valueChanged = true;
+            this->value = value.valueStr;
+            this->type = value.type;
+        }
         return true;
     }
+
+    void handleTimestepEnd(std::vector<std::string> &printBacklog) {
+        if (valueChanged) {
+            valueChanged = false;
+            switch (type) {
+                case vcd_reader<TranslatingSignalWrapper>::SINGLE_BIT:
+                    printBacklog.push_back(value + vcdAlias);
+                    break;
+                case vcd_reader<TranslatingSignalWrapper>::MULTI_BIT:
+                    printBacklog.push_back('b' + value + ' ' + vcdAlias);
+                    break;
+                case vcd_reader<TranslatingSignalWrapper>::REAL:
+                    printBacklog.push_back('r' + value + ' ' + vcdAlias);
+                    break;
+                default:
+                    std::cout << "ERROR: unexpected value type!" << std::endl;
+                    break;
+            }
+        }
+    }
+
+  private:
+    const std::string vcdAlias;
+    std::string value;
+    vcd_reader<TranslatingSignalWrapper>::ValueType type;
+    bool valueChanged;
 };
 
 int main(int argc, char **argv) {
@@ -58,34 +92,31 @@ int main(int argc, char **argv) {
     std::ofstream out(outputFile);
 
     std::map<std::string, std::string> signalNameTranslation;
-    std::map<std::string, std::string> vcdAliasConversion;
 
     for (const auto &inputVcdFile : inputVcdFiles) {
-        vcdAliasConversion.clear();
-
-        vcd_reader<DummySignalWrapper> vcdReader(
+        const bool firstRun = offset == 0;
+        vcd_reader<TranslatingSignalWrapper> vcdReader(
             inputVcdFile,
             [&](const std::stack<std::string> & /*scopes*/,
                 const std::string &signalName, const std::string &vcdAlias,
                 const std::string &typeStr, const std::string &bitwidthStr)
-                -> std::optional<DummySignalWrapper> {
+                -> std::optional<TranslatingSignalWrapper> {
                 // keep everything!
-                if (offset == 0) {
+                if (firstRun) {
                     out << VAR_TOKEN << ' ' << typeStr << ' ' << bitwidthStr
                         << ' ' << vcdAlias << ' ' << signalName << ' '
                         << END_TOKEN << std::endl;
                     signalNameTranslation.emplace(signalName, vcdAlias);
                 } else {
                     auto it = signalNameTranslation.find(signalName);
-                    if (it != signalNameTranslation.end()) {
-                        vcdAliasConversion.emplace(vcdAlias, it->second);
-                    } else {
+                    if (it == signalNameTranslation.end()) {
                         std::cout
                             << "WARNING: can not concat signal: " << signalName
                             << std::endl;
                         return std::nullopt;
                     }
-                    //TODO we actually need a handler here to be able to rewrite value changes for the given vcdAlias to its corresponding previous vcdAlias!
+
+                    return TranslatingSignalWrapper(it->second);
                 }
 
                 return std::nullopt;
@@ -93,8 +124,28 @@ int main(int argc, char **argv) {
             [&](std::vector<std::string> &printBacklog) {
                 // on timestamp end -> patch the timestamp
                 if (!printBacklog.empty()) {
-                    printBacklog[0] = std::string("#") +
-                                      std::to_string(lastTimestamp + offset);
+                    // Check if this is a regular timestamp
+                    if (printBacklog[0][0] == '#') {
+                    patchBacklog:
+                        printBacklog[0] =
+                            std::string("#") +
+                            std::to_string(lastTimestamp + offset);
+                    } else if (printBacklog[0] == DUMPVARS_TOKEN) {
+                        // Value initialization
+                        if (!firstRun) {
+                            // Remove the $end!
+                            printBacklog.pop_back();
+                            goto patchBacklog;
+                        }
+                    } else {
+                        std::cout << "ERROR: unknown timestampEnd cause with "
+                                     "first token: "
+                                  << printBacklog[0] << std::endl;
+                        return;
+                    }
+                }
+                for (auto &[first, second] : vcdReader.getVcdAliasHandler()) {
+                    second.handleTimestepEnd(printBacklog);
                 }
             },
             [&](uint64_t timestamp) {
@@ -103,7 +154,7 @@ int main(int argc, char **argv) {
                 return false;
             },
             [&](const std::string &line, bool isHeader) {
-                if (offset == 0 || !isHeader) {
+                if (firstRun || !isHeader) {
                     out << line << std::endl;
                 }
             });
@@ -113,7 +164,7 @@ int main(int argc, char **argv) {
         }
 
         // run the vcdReader
-        vcdReader.process(false);
+        vcdReader.process(!firstRun);
 
         offset += lastTimestamp;
     }
